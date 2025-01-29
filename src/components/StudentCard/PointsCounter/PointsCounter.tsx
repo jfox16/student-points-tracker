914f9e1405c0e3d971f3bbc63bf3d1e3d3299b93
jfox <jfox16@github.com>
2025-01-28 19:49:48 -0800

    Added color coded points

--- src/components/StudentCard/PointsCounter/PointsCounter.tsx
+++ src/components/StudentCard/PointsCounter/PointsCounter.tsx
@@ -46,15 +46,69 @@ export const PointsCounter = (props: PointsCounterProps) => {
     updatePoints
   ]);
 
+  console.log('color', getDynamicColor(student.points));
+
   return (
     <div className="PointsCounter">
       <div className="plus-minus" onClick={decrement}><span>-</span></div>
       <HoverInput
-        className={cns("points-input", { 'negative': student.points < 0 })} 
+        className={cns("points-input")} 
         value={student.points}
         onChange={onInputChange}
+        inputProps={{
+          style: { color: getDynamicColor(student.points) }
+        }}
       />
       <div className="plus-minus" onClick={increment}><span>+</span></div>
     </div>
   );
+}
+
+/**
+ * Returns a color for student points. From 0-20 it changes colors. black -> green -> blue -> purple -> gold
+ * @param value Student points from 0-20
+ * @returns 
+ */
+function getDynamicColor(value: number) {
+  if (value < 0) {
+    return `rgb(200, 0, 0)`;
+  }
+
+  const transition = value / 20;
+
+  let red = 0;
+  let green = 0;
+  let blue = 0;
+
+  // Black -> Green
+  if (transition <= 0.25) {
+    const factor = transition * 4;
+    green = factor * 160;
+  }
+  else if (transition <= 0.50) {
+    const factor = (transition - 0.25) * 4;
+    green = 160 - factor * 40;
+    blue = factor * 220;
+  }
+  else if (transition <= 0.75) {
+    const factor = (transition - 0.50) * 4;
+    red = factor * 180;
+    green = 120 - factor * 120;
+    blue = 220;
+  }
+  else if (transition <= 1.00) {
+    const factor = (transition - 0.75) * 4;
+    red = 180 + (factor * 20);
+    green = factor * 160;
+    blue = 200 - (factor * 200);
+  }
+  else {
+    red = 200;
+    green = 160;
+    blue = 0;
+  }
+  
+  const color = `rgb(${red}, ${green}, ${blue})`;
+  console.info('color', color);
+  return color;
 }
